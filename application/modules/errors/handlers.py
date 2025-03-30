b905369090df3477456a1a6e618d774c4d982950
Jack Hermanson <jack.hermanson@live.com>
2025-03-30 15:37:31 -0600

    try more things

--- application/modules/errors/handlers.py
+++ application/modules/errors/handlers.py
@@ -1,6 +1,6 @@
 import traceback
 
-from flask import Blueprint, render_template, request, make_response, jsonify
+from flask import Blueprint, render_template
 from werkzeug.exceptions import HTTPException
 
 from application import logger
@@ -9,20 +9,11 @@ errors = Blueprint("errors", __name__)
 
 
 @errors.app_errorhandler(HTTPException)
-@errors.app_errorhandler(Exception)
 def handle_exception(e):
+    logger.debug("AN ERROR OCCURRED")
     logger.error(traceback.format_exc())
     status = e.get_response().status_code
     return render_template("errors/generic-error.html", status=status, error=e.__str__()), status
-
-
-from werkzeug.exceptions import InternalServerError
-
-
-@errors.errorhandler(InternalServerError)
-def handle_internal_server_error(error):
-    original = getattr(error, "original_exception", None)
-    return jsonify({"error": "Internal server error", "details": str(original)}), 500
 
 
 # @errors.app_errorhandler(403)
