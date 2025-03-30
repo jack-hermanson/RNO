64d872143ddbc4958b4e5d3b394c89233bb025cc
Jack Hermanson <jack.hermanson@live.com>
2025-03-30 16:00:04 -0600

    error handling

--- application/modules/errors/handlers.py
+++ application/modules/errors/handlers.py
@@ -1,6 +1,6 @@
 import traceback
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 from werkzeug.exceptions import HTTPException
 
 from application import logger
@@ -8,22 +8,16 @@ from application import logger
 errors = Blueprint("errors", __name__)
 
 
-@errors.app_errorhandler(HTTPException)
-def handle_http_exception(e):
-    logger.debug("AN ERROR OCCURRED")
-    logger.error(traceback.format_exc())
-    status = e.get_response().status_code
-    return render_template("errors/generic-error.html", status=status, error=e.__str__()), status
-
-
 @errors.app_errorhandler(Exception)
-def handle_generic_exception(e):
+def handle_http_exception(e):
+    if current_app.debug:  # If debugging, re-raise the error to let Flask handle it
+        raise e
+
     logger.debug("AN ERROR OCCURRED")
     logger.error(traceback.format_exc())
+
     status = 500
-    return render_template("errors/generic-error.html", status=status, error=e.__str__()), status
+    if isinstance(e, HTTPException):
+        status = e.code
 
-
-# @errors.app_errorhandler(403)
-# def error_403(error):
-#     return render_template("errors/generic-error.html", error=error, status=403), 403
+    return render_template("errors/generic-error.html", status=status, error=str(e)), status
